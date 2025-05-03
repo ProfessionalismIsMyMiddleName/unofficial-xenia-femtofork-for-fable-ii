ab2880a1da3d77ca9edf4aee6f04e0031886170a
Harry Gillanders <contact@harrygillanders.com>
2025-05-03 02:39:50 +0100

    Make some private state public.

--- src/xenia/gpu/d3d12/d3d12_texture_cache.h
+++ src/xenia/gpu/d3d12/d3d12_texture_cache.h
@@ -544,7 +544,7 @@ class D3D12TextureCache final : public TextureCache {
 
   void UpdateTextureBindingsImpl(uint32_t fetch_constant_mask) override;
 
- private:
+ public:
   static constexpr uint32_t kLoadGuestXThreadsPerGroupLog2 = 2;
   static constexpr uint32_t kLoadGuestYBlocksPerGroupLog2 = 5;
 
@@ -672,7 +672,7 @@ class D3D12TextureCache final : public TextureCache {
     // After an aliasing barrier (which is even stronger than an UAV barrier).
     void ClearUAVBarrierPending() { uav_barrier_pending_ = false; }
 
-   private:
+   public:
     Microsoft::WRL::ComPtr<ID3D12Resource> resource_;
     D3D12_RESOURCE_STATES resource_state_;
     bool uav_barrier_pending_ = false;
