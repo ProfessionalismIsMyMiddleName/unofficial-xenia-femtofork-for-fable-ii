99488f6926b2152d7ccdd3db5515444d8f72be51
Harry Gillanders <contact@harrygillanders.com>
2025-05-13 16:19:25 +0100

    Implement automatic backups of save games.
    To guard against save file corruption.

---
+++ src/xenia/f2/features.cc
@@ -0,0 +1,16 @@
+
+#include "xenia/f2/features.h"
+
+namespace xe {
+namespace f2 {
+/* Accesses to this data could theoretically race--but in practice they won't.
+ */
+SaveFileBackupState<7> active_save_file_backup = {
+    /* The code responsible for backing up the save files, in
+       `NtCreateFile_entry`, relies on each of these paths beginning with a
+       prefix of 'save:\'. */
+    {"save:\\chaptersave.bin", "save:\\Fable2PubInfo.xml",
+     "save:\\failquestsave.bin", "save:\\herosave.bin", "save:\\mainsave.bin",
+     "save:\\saveuid.bin", "save:\\texturemorphs.bin"}};
+}  // namespace f2
+}  // namespace xe
